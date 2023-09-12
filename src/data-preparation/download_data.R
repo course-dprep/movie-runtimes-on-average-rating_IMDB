8d30b9fef306e5c073f57ffc375c3e7153f12ab2
NCPEValen <120317378+NCPEValen@users.noreply.github.com>
2023-09-12 11:52:42 +0200

    attempt 2 of committing the change dowload data file

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -1,8 +1,11 @@
 # Download dataset 1
 # dir.create('./data/dataset1')  # Uncomment if need to create directory with R
-download.file('https://datasets.imdbws.com/title.basics.tsv.gz', destfile = filename)
+download.file('https://datasets.imdbws.com/title.basics.tsv.gz', "title_basics.tsv")
+library(tidyverse)
+title_basics <- read_tsv("title_basics.tsv", n_max = 100)
+View(title_basics)
 
 # Download dataset 2
 # dir.create('./data/dataset2')  # Uncomment if need to create directory with R
-download.file('https://datasets.imdbws.com/title.ratings.tsv.gz', destfile = filename)
-
+download.file('https://datasets.imdbws.com/title.ratings.tsv.gz', "title_ratings.tsv")
+title_ratings <- read_tsv("title_ratings.tsv", n_max = 100)
