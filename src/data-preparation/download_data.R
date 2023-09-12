3a541e3adb5fc8944f493f4747e3e4bb4f9a6719
NCPEValen <120317378+NCPEValen@users.noreply.github.com>
2023-09-12 11:28:10 +0200

    changed file to download data

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -1,8 +1,8 @@
 # Download dataset 1
 # dir.create('./data/dataset1')  # Uncomment if need to create directory with R
-download.file('https://datasets.imdbws.com/title.basics.tsv.gz')
+download.file('https://datasets.imdbws.com/title.basics.tsv.gz', destfile = filename)
 
 # Download dataset 2
 # dir.create('./data/dataset2')  # Uncomment if need to create directory with R
-download.file('https://datasets.imdbws.com/title.ratings.tsv.gz')
+download.file('https://datasets.imdbws.com/title.ratings.tsv.gz', destfile = filename)
 
