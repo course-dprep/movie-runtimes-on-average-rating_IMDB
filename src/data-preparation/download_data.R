9a992f308822854fe30aaf2d704fe975623704ae
lindseycoover <lincoover@gmail.com>
2023-09-22 16:46:53 +0200

    Switch v2 code to original files so v2 versions can be deleted for housekeeping

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -1,11 +1,18 @@
-# Download dataset 1
-# dir.create('./data/dataset1')  # Uncomment if need to create directory with R
-download.file('https://datasets.imdbws.com/title.basics.tsv.gz', "title_basics.tsv")
+library(dplyr)
 library(tidyverse)
-title_basics <- read_tsv("title_basics.tsv", n_max = 100)
-View(title_basics)
 
-# Download dataset 2
-# dir.create('./data/dataset2')  # Uncomment if need to create directory with R
-download.file('https://datasets.imdbws.com/title.ratings.tsv.gz', "title_ratings.tsv")
-title_ratings <- read_tsv("title_ratings.tsv", n_max = 100)
+#Create a function download_data which downloads files from URLs. 
+#Downloaded data assigned filename, storage path, and URL. 
+download_data <- function(url, filename, filepath) {
+  # Create directory if it doesn't exist
+  if (!dir.exists(filepath)) {
+    dir.create(filepath)
+  }
+  
+  # Download file
+  download.file(url = url, destfile = paste0(filepath, "/", filename))
+}
+
+#Use the function to download the datasets from IMDB and put into correct location
+download_data(url = "https://datasets.imdbws.com/title.basics.tsv.gz", filename = "title_basics.tsv", filepath = "../../data/dataset1")
+download_data(url = "https://datasets.imdbws.com/title.ratings.tsv.gz", filename = "title_ratings.tsv", filepath = "../../data/dataset2")
