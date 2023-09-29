dca590c561ef5d3daca36600471a521bca8ddd49
fleurvanvugt <fleur151@live.nl>
2023-09-29 15:17:20 +0200

    Fixed linear regression and changed homoskedasticity check to be fitting for the integer and numeric values

--- src/analysis/model_results.R
+++ src/analysis/model_results.R
@@ -1,4 +1,4 @@
-
+library(lmtest)
 ## SETUP
 library(dplyr)
 library(car)
@@ -10,9 +10,11 @@ read_csv("../../gen/data-preparation/temp/data_cleaned.csv")
 ## Linear regression
 # Research question: What is the relationship between the runtime and average user rating for movies?
 imdb_lm1 <- lm(averageRating ~ runtimeMinutes , data_cleaned)
+summary(imdb_lm1)
 
 # Assumption check: test for homoskedasticity
-leveneTest(averageRating ~ runtimeMinutes, mergeddata, center=mean) # significant p-value means violated assumption of homoskedasticity
+breuschpagan_test <- lm(averageRating ~ runtimeMinutes, data = data_cleaned) %>% bptest()
+breuschpagan_test
 
 # Assumption check: test for independence of observations
 dwtest(lm1) # a value near 2 suggests independence
