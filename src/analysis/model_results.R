2d80553f265fb0a06f02daf11083c703ffc4a8cb
NCPEValen <120317378+NCPEValen@users.noreply.github.com>
2023-10-06 23:23:15 +0200

    Comments with the assumptions to explain results and suggestion for heteroskedasticity

--- src/analysis/model_results.R
+++ src/analysis/model_results.R
@@ -20,15 +20,20 @@ summary(imdb_lm2)
 # Assumption check: test for homoskedasticity
 breuschpagan_test <- lm(averageRating ~ runtimeMinutes, data = data_cleaned) %>% bptest()
 breuschpagan_test
+# We reject the null-hypothesis that there is constant variance and identify the presence of heteroskedasticity in our data.
+# For future research, we suggest using robust standard errors when working with the data. 
 
 # Assumption check: test for independence of observations
 dwtest(imdb_lm1) # a value near 2 suggests independence
+# The Durbin-Watsen statistic is 1.9367 and the p-value is significant at the 0.05 level. This suggests that there is no autocorrelation in the residuals. 
 
 # Assumption check: test for normality
-qqnorm(data_cleaned$averageRating) # relatively straight line indicates normality
+qqnorm(data_cleaned$averageRating) 
+# The relatively straight line indicates normality
 
 # Assumption check: test for linearity
 plot(data_cleaned$runtimeMinutes, data_cleaned$averageRating, xlab = "Runtime Minutes", ylab = "Average Rating", main = "Scatterplot of Average Rating vs. Runtime Minutes")
+# The resulting plot suggests no problematic multicollinearity. 
 
 ## OUTPUT
 # Save output
