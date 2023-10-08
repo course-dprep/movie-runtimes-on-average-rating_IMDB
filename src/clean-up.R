2fa144b84076041785235c3c7e1b31b6106e17bf
lindseycoover <lincoover@gmail.com>
2023-10-08 16:52:51 +0200

    created makefile for src/data-preparation and adjusted clean-up.R in src

--- src/clean-up.R
+++ src/clean-up.R
@@ -1,5 +1,5 @@
-# Deletes files in all subdirectories with the endings specified here
-fileEndings <- c('*.log','*.aux','*.Rout','*.Rhistory','*.fls','*.fdb_latexmk')
+# Deletes files in all sub-directories with the endings specified here
+fileEndings <- c('*.log','*.aux','*.Rout','*.Rhistory','*.fls','*.fdb_latexmk', '*.tsv', '*.csv')
 for (fi in fileEndings) { 
   files <- list.files(getwd(),fi,include.dirs=F,recursive=T,full.names=T,all.files=T)
   file.remove(files)
@@ -7,9 +7,10 @@ for (fi in fileEndings) {
 
 # Delete all files in temp directories
 # (does note delete hidden files starting with . (e.g. .gitkeep is not deleted))
-unlink(paste(getwd(),'/gen/analysis/temp/*',sep=''),recursive=T,force=T)
-unlink(paste(getwd(),'/gen/data-preparation/temp/*',sep=''),recursive=T,force=T)
-unlink(paste(getwd(),'/gen/paper/temp/*',sep=''),recursive=T,force=T)
+unlink(paste(getwd(),'../gen/analysis/temp/*',sep=''),recursive=T,force=T)
+unlink(paste(getwd(),'../gen/data-preparation/temp/*',sep=''),recursive=T,force=T)
+unlink(paste(getwd(),'../gen/paper/temp/*',sep=''),recursive=T,force=T)
+unlink(paste(getwd(),'../data/dataset/*',sep=''),recursive=T,force=T)
 
 # Delete temporary (hidden) R files
 file.remove('.RData')
--- src/data-preparation/merge_data.R
+++ src/data-preparation/merge_data.R
@@ -3,8 +3,8 @@ library(tidyverse)
 
 ## INPUT
 #Load the data into R
-title_basics <- read_tsv("../../data/dataset/title_basics.tsv")
-title_ratings <- read_tsv("../../data/dataset/title_ratings.tsv")
+title_basics <- read_tsv("../../data/datasets/title_basics.tsv")
+title_ratings <- read_tsv("../../data/datasets/title_ratings.tsv")
 
 ## TRANSFORMATION
 # Combine data by tconst 
